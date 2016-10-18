6d088413b1903625e699dcd3b1aae583494f6eab
Ian Russell <ian.andrew.russell@gmail.com>
2016-10-18 14:35:39 -0400

    prgrm complted - determies what year subject was born

--- excercise3.rb
+++ excercise3.rb
@@ -1,5 +1,7 @@
-
-
 puts "what is your name?"
 name=gets.chomp
 puts "Hi #{name}"
+puts "How old are you?"
+age=gets.chomp.to_i
+birth_year=2016-age
+puts "So you are #{age} and wouldd have been born in #{birth_year}"
